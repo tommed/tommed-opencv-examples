9f018cc6c2b7a235231a4594a936e0b988ba4074
Tom Medhurst <tom.medhurst@gmail.com>
2009-04-10 21:25:53 +0100

    moved facial recognition to keep fs tidy

--- videotest.c
+++ videotest.c
@@ -8,7 +8,7 @@
 int main(int argc, char *argv[]) {
 
 	if (argc<2) {
-		fprintf(stderr, "You must specify the path to an .avi file\n");
+		fprintf(stderr, "You must specify the path to a video file\n");
 		exit(1);
 	}
 	
